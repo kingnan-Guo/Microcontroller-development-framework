7db085a335be39903bab295352f34de85822de58
kingnan <373734199@qq.com>
2024-01-11 03:32:32 +0800

    在 C调用C++ 时一直报错

--- src/main.cpp
+++ src/main.cpp
@@ -15,3 +15,24 @@ int main(){
     // fmt::print("Hello, world!");
     return 0;
 }
+
+
+
+// #include <stdio.h>
+// // #include <fmt/format.h>
+// #include <print.hpp>
+// #include <evolution.h>
+// #include <iostream>
+// // extern "C" {
+// //     #include <evolution.h>
+// // }
+
+// int main(){
+
+//     // printf("fmt version : %d\n", FMT_VERSION);
+//     printf("Hello, world main.c!\n");
+//     print();
+//     evo_fn();
+//     // fmt::print("Hello, world!");
+//     return 0;
+// }
