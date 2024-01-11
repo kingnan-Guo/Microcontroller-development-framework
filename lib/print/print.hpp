206799211eb81c05f8f5851e5e329ceaef366e3a
kingnan-Guo <373734199@qq.com>
2024-01-11 14:37:34 +0800

    新的目录结构
    1、将 lib 包分包而不是放在 一个文件家里
    2、未来会有很多的lib

---
+++ lib/print/print.hpp
@@ -0,0 +1,6 @@
+#ifndef __PRINT_H__
+#define __PRINT_H__
+#include <iostream>
+using namespace std;
+void print();
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <fmt/format.h>
+// #include <fmt/format.h>
 #include <print.hpp>
 // #include <evolution.h>
 extern "C" {
@@ -7,32 +7,11 @@ extern "C" {
 }
 
 int main(){
-    std::cout << fmt::format("Hello, {}!", "world") << std::endl;
-    std::cout << fmt::format("fmt version : {}", FMT_VERSION) << std::endl;
+    // std::cout << fmt::format("Hello, {}!", "world") << std::endl;
+    // std::cout << fmt::format("fmt version : {}", FMT_VERSION) << std::endl;
     std::cout << "Hello, world!" << std::endl;
     print();
     evo_fn();
     // fmt::print("Hello, world!");
     return 0;
 }
-
-
-
-// #include <stdio.h>
-// // #include <fmt/format.h>
-// #include <print.hpp>
-// #include <evolution.h>
-// #include <iostream>
-// // extern "C" {
-// //     #include <evolution.h>
-// // }
-
-// int main(){
-
-//     // printf("fmt version : %d\n", FMT_VERSION);
-//     printf("Hello, world main.c!\n");
-//     print();
-//     evo_fn();
-//     // fmt::print("Hello, world!");
-//     return 0;
-// }
