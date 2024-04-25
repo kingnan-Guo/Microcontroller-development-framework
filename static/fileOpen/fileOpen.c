f79a2641d2191201ee0e13b409701f31631ce2d5
kingnan <373734199@qq.com>
2024-04-26 00:25:22 +0800

    读写 文件

--- static/fileOpen/fileOpen.c
+++ static/fileOpen/fileOpen.c
@@ -7,14 +7,32 @@
 #include "string.h"
 
 
-
-void fileOpenMain(void){
-    printf("====== fileOpenMain ========\n");
-
+void writeFile(void){
     FILE * f = fopen("/Users/kingnan/Documents/github/Microcontroller-development-framework/file/text.txt", "w");
     fputc('A', f);
     fputs("fileOpenMain2", f);
     fclose(f);
+}
+
+
+void readFile(void){
+    char fileStr;
+    char strs[10];
+    FILE * f = fopen("/Users/kingnan/Documents/github/Microcontroller-development-framework/file/text.txt", "r");
+    fileStr = fgetc(f);
+
+    fgets(strs, 10, f);
+    fclose(f);
+    printf("fileStr = %c\n",fileStr);
+    printf(strs);
+}
+
+void fileOpenMain(void){
+    printf("====== fileOpenMain ========\n");
+
+//    writeFile();
+
+    readFile();
 
     return;
 }
